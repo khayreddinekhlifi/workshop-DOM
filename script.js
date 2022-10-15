0669c1893f542ca740075fe5cfd66a03a9d06fc1
khayreddinekhlifi <khayrikhlifi21@gmail.com>
2022-10-15 12:48:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuhamburger = document.getElementById("menu-hamburger");
+
+const lien = document.getElementById("mon-lien");
+menuhamburger.addEventListener("click", () => {
+  lien.classList.toggle("show");
+});
+var boutton = document.getElementById("btn");
+function click() {}
